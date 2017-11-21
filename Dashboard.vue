c52a5860161673be2905b4dbcc50001ffdcdaa8d
sleepy-hollow <33806779+sleepy-hollow@users.noreply.github.com>
2017-11-22 01:17:17 +0900

    Add files via upload

---
+++ Dashboard.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="dashboard">
+    <vuestic-widget headerText="SuggestBox">
+      value: {{ value }}
+      <div class="www">
+        <suggest-box :suggestions="suggestList" v-model="value"></suggest-box>
+      </div>
+    </vuestic-widget>
+  </div>
+</template>
+
+<script>
+  import SuggestBox from '../SuggestBox.vue'
+
+  export default {
+    name: 'dashboard',
+    components: {
+      SuggestBox
+    },
+    data () {
+      return {
+        value: '',
+        suggestList: [
+          { name: '夏休み' },
+          { name: 'アッコ' },
+          { name: 'ロッテ' },
+          { name: 'スーシー' },
+          { name: 'ダイアナ' },
+          { name: 'リンゴ' },
+          { name: 'もも' },
+          { name: '春' },
+          { name: '池' },
+          { name: '雪' },
+          { name: '夏休み' }
+        ]
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import "../../sass/_variables.scss";
+  .www {
+    width: 300px;
+  }
+</style>
